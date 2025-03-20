e42363fe0de3fbdf72e3d6bbbd71e21fbcf6c9fc
Jadhav-Nandini <nandinij780@gmail.com>
2025-03-21 01:01:41 +0530

    created frontend routes and design navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path = '/' element ={<Home/>} /> 
+      <Route path = '/login' element ={<Login/>} /> 
+      <Route path = '/ResetPassword' element ={<ResetPassword/>} /> 
+      <Route path = '/EmailVerify' element ={<EmailVerify/>} /> 
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/assets/assets.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg"
+import hand_wave from "./hand_wave.jpg"
+import menu from "./menu.svg"
+
+export const assets = {
+    logo,
+    hand_wave,
+    menu
+}
